b3e6fbd8d3097501b24c14a7faaa5556cd2044da
Adham AL Azzabi <79052745+AdhamAzzbabi@users.noreply.github.com>
2022-11-20 00:18:56 +0200

    Create hotel.sql

--- hotel/hotel.sql
+++ hotel/hotel.sql
@@ -1,12 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: May 17, 2022 at 07:40 PM
--- Server version: 10.1.19-MariaDB
--- PHP Version: 5.6.24
--- Edited By: Waleed Ibrahim
+//Edited By:Adham
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
